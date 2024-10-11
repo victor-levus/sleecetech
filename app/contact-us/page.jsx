c423e38db75b5d90097490308d5552633fc6dbe8
Victor Umedum <victorumedum@gmail.com>
2024-10-11 11:28:38 +0100

    edit the google map iframe

--- app/contact-us/page.jsx
+++ app/contact-us/page.jsx
@@ -221,7 +221,7 @@ export default function ContactUsPage() {
 							<CardContent>
 								<div className="aspect-video relative rounded-md overflow-hidden">
 									<iframe
-										src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d10598.943698161833!2d7.473732106799257!3d9.094810698359245!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sen!2sng!4v1728641306783!5m2!1sen!2sng"
+										src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1969.8072170893022!2d7.471414791961231!3d9.098843315015353!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104e0b17bda1d00b%3A0xe8d76aaf46002194!2skatampe%20main!5e0!3m2!1sen!2sng!4v1728642413031!5m2!1sen!2sng"
 										width="100%"
 										height="100%"
 										style={{ border: 0 }}
