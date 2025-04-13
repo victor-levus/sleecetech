3d85cc54aa4ebc7dd07db283841c469eff777901
victor-levus <95586250+victor-levus@users.noreply.github.com>
2025-04-13 18:45:40 +0100

    fix image in services card

--- data/index.js
+++ data/index.js
@@ -1,124 +1,124 @@
 export const servicesData = [
-  {
-    title: "IT Consulting",
-    subTitle:
-      "Offering expert advice and strategic guidance to help businesses optimize their IT systems, improve efficiency, and align technology with their goals",
-    image:
-      "https://img.freepik.com/premium-photo/providing-business-consulting-services-guiding-clients-ai-generated_547674-1107.jpg",
-  },
-  {
-    title: "Networking",
-    subTitle:
-      "Designing, implementing, and maintaining secure and efficient network solutions to ensure seamless communication and data exchange within organizations",
-    image:
-      "https://miro.medium.com/v2/resize:fit:1024/1*hdjgfeXvokmq3Oe_wyvCEw.jpeg",
-  },
-  {
-    title: "IT Infrastructure Services",
-    subTitle:
-      "Providing comprehensive management of IT infrastructure, including hardware, software, and network components, to ensure reliability and performance",
-    image:
-      "https://img.freepik.com/premium-photo/cloud-computing-infrastructure-ai-generated_547674-1577.jpg",
-  },
-  {
-    title: "Power Installation",
-    subTitle:
-      "Installing and managing power solutions, including UPS systems and backup power supplies, to ensure continuous and reliable operation of IT systems.",
-    image:
-      "https://img.freepik.com/premium-photo/man-blue-shirt-working-solar-panel-he-tendency-take-advantage-free-roofs-industries-place-photovoltaic-panels-reduce-business-electricity-costs-ai-generated_538213-47068.jpg",
-  },
-  {
-    title: "Managed Services",
-    subTitle:
-      "Delivering proactive IT management and support, including monitoring, maintenance, and troubleshooting, to ensure optimal performance and minimize downtime",
-    image:
-      "https://img.freepik.com/premium-photo/3d-illustration-business-people-working-office-3d-rendering-exploring-evolving-interplay-business-ai-generated_538213-10349.jpg",
-  },
-  {
-    title: "Telecommunication",
-    subTitle:
-      "Offering telecommunication solutions, such as VoIP, video conferencing, and unified communication systems, to enhance business communication and collaboration",
-    image:
-      "https://media.licdn.com/dms/image/C5612AQGW1hysHX0Uwg/article-cover_image-shrink_720_1280/0/1520096144505?e=2147483647&v=beta&t=jE1WtKOLSX4E5Aym464aG83MpXB3YYKpuimXrWghkFw",
-  },
-  {
-    title: "Software Development",
-    subTitle:
-      "Developing custom software applications tailored to meet specific business needs, from web and mobile apps to enterprise solutions",
-    image:
-      "https://devsdata.com/wp-content/uploads/2024/05/5-interesting-facts-about-software-developers-DevsData.jpg",
-  },
-  {
-    title: "Cloud Computing",
-    subTitle:
-      "Providing cloud services, including migration, management, and optimization, to help businesses leverage the scalability, flexibility, and cost-efficiency of cloud-based solutions",
-    image:
-      "https://static.vecteezy.com/system/resources/previews/035/571/609/non_2x/ai-generated-abstract-cloud-computing-technology-concept-generative-ai-free-photo.jpg",
-  },
+	{
+		title: "IT Consulting",
+		subTitle:
+			"Offering expert advice and strategic guidance to help businesses optimize their IT systems, improve efficiency, and align technology with their goals",
+		image:
+			"https://img.freepik.com/premium-photo/providing-business-consulting-services-guiding-clients-ai-generated_547674-1107.jpg",
+	},
+	{
+		title: "Networking",
+		subTitle:
+			"Designing, implementing, and maintaining secure and efficient network solutions to ensure seamless communication and data exchange within organizations",
+		image:
+			"https://miro.medium.com/v2/resize:fit:1024/1*hdjgfeXvokmq3Oe_wyvCEw.jpeg",
+	},
+	{
+		title: "IT Infrastructure Services",
+		subTitle:
+			"Providing comprehensive management of IT infrastructure, including hardware, software, and network components, to ensure reliability and performance",
+		image:
+			"https://img.freepik.com/premium-photo/cloud-computing-infrastructure-ai-generated_547674-1577.jpg",
+	},
+	{
+		title: "Power Installation",
+		subTitle:
+			"Installing and managing power solutions, including UPS systems and backup power supplies, to ensure continuous and reliable operation of IT systems.",
+		image:
+			"https://img.freepik.com/premium-photo/man-blue-shirt-working-solar-panel-he-tendency-take-advantage-free-roofs-industries-place-photovoltaic-panels-reduce-business-electricity-costs-ai-generated_538213-47068.jpg",
+	},
+	{
+		title: "Managed Services",
+		subTitle:
+			"Delivering proactive IT management and support, including monitoring, maintenance, and troubleshooting, to ensure optimal performance and minimize downtime",
+		image:
+			"https://img.freepik.com/premium-photo/3d-illustration-business-people-working-office-3d-rendering-exploring-evolving-interplay-business-ai-generated_538213-10349.jpg",
+	},
+	{
+		title: "Telecommunication",
+		subTitle:
+			"Offering telecommunication solutions, such as VoIP, video conferencing, and unified communication systems, to enhance business communication and collaboration",
+		image:
+			"https://media.licdn.com/dms/image/C5612AQGW1hysHX0Uwg/article-cover_image-shrink_720_1280/0/1520096144505?e=2147483647&v=beta&t=jE1WtKOLSX4E5Aym464aG83MpXB3YYKpuimXrWghkFw",
+	},
+	{
+		title: "Software Development",
+		subTitle:
+			"Developing custom software applications tailored to meet specific business needs, from web and mobile apps to enterprise solutions",
+		image:
+			"https://www.flexsin.com/blog/wp-content/uploads/2019/12/Custom-Software-Development.jpg",
+	},
+	{
+		title: "Cloud Computing",
+		subTitle:
+			"Providing cloud services, including migration, management, and optimization, to help businesses leverage the scalability, flexibility, and cost-efficiency of cloud-based solutions",
+		image:
+			"https://static.vecteezy.com/system/resources/previews/035/571/609/non_2x/ai-generated-abstract-cloud-computing-technology-concept-generative-ai-free-photo.jpg",
+	},
 
-  {
-    title: "Cybersecurity",
-    subTitle:
-      "Implementing and managing robust cybersecurity measures to protect businesses from threats such as malware, phishing, and data breaches",
-    image:
-      "https://www.innovationnewsnetwork.com/wp-content/uploads/2023/08/shutterstockBlue-Planet-Studio_1960378399-696x392.jpg",
-  },
+	{
+		title: "Cybersecurity",
+		subTitle:
+			"Implementing and managing robust cybersecurity measures to protect businesses from threats such as malware, phishing, and data breaches",
+		image:
+			"https://www.innovationnewsnetwork.com/wp-content/uploads/2023/08/shutterstockBlue-Planet-Studio_1960378399-696x392.jpg",
+	},
 ];
 
 export const expertiseData = [
-  {
-    title: "Comprehensive Expertise",
-    subTitle:
-      "Our diverse portfolio covers everything from Networking and Telecom to Software Development, Solar Power Installation, Managed Services, and IT Infrastructure. We offer a one-stop solution for all your IT needs, ensuring seamless integration and consistent performance",
-    image:
-      "https://www.sapbwconsulting.com/hubfs/Learn%20SEO%20from%20Experts/Learning%20SEO%20from%20the%20Experts.png",
-  },
+	{
+		title: "Comprehensive Expertise",
+		subTitle:
+			"Our diverse portfolio covers everything from Networking and Telecom to Software Development, Solar Power Installation, Managed Services, and IT Infrastructure. We offer a one-stop solution for all your IT needs, ensuring seamless integration and consistent performance",
+		image:
+			"https://www.sapbwconsulting.com/hubfs/Learn%20SEO%20from%20Experts/Learning%20SEO%20from%20the%20Experts.png",
+	},
 
-  {
-    title: "Tailored Solutions",
-    subTitle:
-      "We understand that every business is unique. That's why we customize our services to meet your specific requirements, ensuring that our solutions are perfectly aligned with your strategic goals",
-    image:
-      "https://media.licdn.com/dms/image/D5612AQE1KeHtMyatwQ/article-cover_image-shrink_720_1280/0/1720274032291?e=2147483647&v=beta&t=dFgoirwjj9MzCOv8aWY95XE1baCwNJ_MAcgkuzD2-hg",
-  },
+	{
+		title: "Tailored Solutions",
+		subTitle:
+			"We understand that every business is unique. That's why we customize our services to meet your specific requirements, ensuring that our solutions are perfectly aligned with your strategic goals",
+		image:
+			"https://media.licdn.com/dms/image/D5612AQE1KeHtMyatwQ/article-cover_image-shrink_720_1280/0/1720274032291?e=2147483647&v=beta&t=dFgoirwjj9MzCOv8aWY95XE1baCwNJ_MAcgkuzD2-hg",
+	},
 
-  {
-    title: "Cutting-Edge Technology",
-    subTitle:
-      "We leverage the latest technologies to provide scalable, secure, and future-proof solutions. Whether it's developing a custom software application or setting up a robust network infrastructure, we ensure you're always ahead of the curve",
-    image:
-      "https://media.licdn.com/dms/image/D5612AQFBmq32Rs7-Xg/article-cover_image-shrink_720_1280/0/1667855552359?e=2147483647&v=beta&t=GinktlTZaJnYk9ztVOXbMzUxAbAeHGnNLzaQv8pgjsE",
-  },
+	{
+		title: "Cutting-Edge Technology",
+		subTitle:
+			"We leverage the latest technologies to provide scalable, secure, and future-proof solutions. Whether it's developing a custom software application or setting up a robust network infrastructure, we ensure you're always ahead of the curve",
+		image:
+			"https://media.licdn.com/dms/image/D5612AQFBmq32Rs7-Xg/article-cover_image-shrink_720_1280/0/1667855552359?e=2147483647&v=beta&t=GinktlTZaJnYk9ztVOXbMzUxAbAeHGnNLzaQv8pgjsE",
+	},
 
-  {
-    title: "Proven Track Record",
-    subTitle:
-      "With years of experience and a portfolio of successful projects, we have built a reputation for excellence. Our clients trust us to deliver reliable and high-quality services that exceed expectations",
-    image:
-      "https://img2.storyblok.com/1000x0/f/81103/1183x791/513b915a1c/undraw_data_trends_re_2cdy.png",
-  },
+	{
+		title: "Proven Track Record",
+		subTitle:
+			"With years of experience and a portfolio of successful projects, we have built a reputation for excellence. Our clients trust us to deliver reliable and high-quality services that exceed expectations",
+		image:
+			"https://img2.storyblok.com/1000x0/f/81103/1183x791/513b915a1c/undraw_data_trends_re_2cdy.png",
+	},
 
-  {
-    title: "Dedicated Support",
-    subTitle:
-      "Our commitment to customer satisfaction is unwavering. We provide continuous support and maintenance, ensuring that your systems run smoothly and any issues are promptly resolved",
-    image:
-      "https://img.freepik.com/free-vector/organic-flat-customer-support_23-2148881015.jpg",
-  },
+	{
+		title: "Dedicated Support",
+		subTitle:
+			"Our commitment to customer satisfaction is unwavering. We provide continuous support and maintenance, ensuring that your systems run smoothly and any issues are promptly resolved",
+		image:
+			"https://img.freepik.com/free-vector/organic-flat-customer-support_23-2148881015.jpg",
+	},
 
-  {
-    title: "Sustainable Solutions",
-    subTitle:
-      "As a forward-thinking company, we incorporate sustainable practices into our operations. Our solar power installations not only reduce energy costs but also contribute to a greener planet",
-    image:
-      "https://experionglobal.com/wp-content/uploads/2024/04/Importance-of-Tailored-Solutions-1.png",
-  },
+	{
+		title: "Sustainable Solutions",
+		subTitle:
+			"As a forward-thinking company, we incorporate sustainable practices into our operations. Our solar power installations not only reduce energy costs but also contribute to a greener planet",
+		image:
+			"https://experionglobal.com/wp-content/uploads/2024/04/Importance-of-Tailored-Solutions-1.png",
+	},
 
-  {
-    title: "Innovative Approach",
-    subTitle:
-      "At Sleece Technologies, innovation is at the heart of everything we do. We constantly explore new technologies and methodologies to enhance our service offerings and deliver greater value to our clients",
-    image:
-      "https://static.vecteezy.com/system/resources/previews/029/306/004/original/team-work-and-team-building-corporate-organization-and-partnership-problem-solving-innovative-business-approach-brainstorming-flat-modern-illustration-vector.jpg",
-  },
+	{
+		title: "Innovative Approach",
+		subTitle:
+			"At Sleece Technologies, innovation is at the heart of everything we do. We constantly explore new technologies and methodologies to enhance our service offerings and deliver greater value to our clients",
+		image:
+			"https://static.vecteezy.com/system/resources/previews/029/306/004/original/team-work-and-team-building-corporate-organization-and-partnership-problem-solving-innovative-business-approach-brainstorming-flat-modern-illustration-vector.jpg",
+	},
 ];
